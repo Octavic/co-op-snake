fd9cc4376a679fd06283ebb0a06fc8a531cd761c
YifanXu <yifanxu00@outlook.com>
2021-06-11 22:35:47 -0700

    Add file importing support

--- Assets/Scripts/InternalScripts/TileTypes.cs
+++ Assets/Scripts/InternalScripts/TileTypes.cs
@@ -6,8 +6,25 @@ using System.Threading.Tasks;
 
 public static class TileTypes
 {
-    public abstract class BaseTile
+    public abstract class Tile
     {
-        public abstract Coordinate Activate();
+        public abstract void Activate(PlayerController player);
+    }
+
+    public class Wall: Tile
+    {
+        public override void Activate (PlayerController player)
+        {
+            return;
+        }
+    }
+
+    public class Star : Tile
+    {
+        public string color;
+        public override void Activate(PlayerController player)
+        {
+            return;
+        }
     }
 }
