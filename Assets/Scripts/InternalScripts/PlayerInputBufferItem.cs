c67127758d661b81b0aae95fb1a35a84a90e23e9
Octavic <yifeixu@live.com>
2021-06-12 16:31:02 -0700

    Add input buffer

---
+++ Assets/Scripts/InternalScripts/PlayerInputBufferItem.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public struct PlayerInputBufferItem
+{
+    public int KeycodeId;
+    public float Time;
+
+    public PlayerInputBufferItem(int keycodeId, float time)
+    {
+        this.KeycodeId = keycodeId;
+        this.Time = time;
+    }
+}
