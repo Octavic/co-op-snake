5a36bfad2f6923e0f70aa57fc99b1ec0416c86c5
Octavic <yifeixu@live.com>
2021-06-12 17:27:13 -0700

    Yifei overlay (#9)
    * Add starting overlay
    
    * Add game over overlay

---
+++ Assets/Scripts/UI/GameOverOverlay.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverOverlay : MonoBehaviour
+{
+    public Text ScoreText;
+    public void Show(float score)
+    {
+        this.gameObject.SetActive(true);
+        this.ScoreText.text = "SCORE: " + score.ToString();
+    }
+
+    public void Hide()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
